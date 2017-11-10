7e8bccb6cf789ad2efe2cc0130c6fb2573d691bc
Jared <jaredjkim@gmail.com>
2017-11-10 05:26:36 -0800

    first commit

---
+++ IDoDeclare.js
@@ -0,0 +1,22 @@
+var Number1 = 29;
+var Number2 = 100.1;
+var Number3 = 29.23;
+var Number4 = -19;
+var string1 = "Hello";
+var string2 = "bye";
+var string3 = "tim";
+var boolean = true;
+var boolean = false;
+var jimmy;
+
+console.log("This is first number:", Number1);
+console.log("This is second number:", Number1);
+console.log("This is third number:", Number1);
+console.log("This is forth number:", Number1);
+console.log("say", string1);
+console.log("bye", string2);
+console.log("who?", string3);
+console.log("is Number2 bigger than 0?", Number2 > 0);
+console.log("is his name tim?", string3 == string3);
+console.log("Who is Jimmy?", jimmy);
+
